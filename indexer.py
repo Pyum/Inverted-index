f589089e1c4685a22831a9a7a742868d7361153b
Brian Caballero <briancaballero7@gmail.com>
2023-02-15 17:51:09 -0800

    Worked on Inverted Index & TODO

--- indexer.py
+++ indexer.py
@@ -1,14 +1,25 @@
+from collections import defaultdict
 
 def createInvertedIndex(_documentSet):
-    _invertedIndex = list()
+    _invertedIndex = defaultdict({"freq": 1, "list":[]})
     _docID = 0
     for _document in _documentSet:
         _docID += 1
         _tokenList = tokenize(_document)
-        
+        for _tokenWord in _tokenList:
+            if _tokenWord in _invertedIndex:
+                _invertedIndex[_tokenWord]["freq"] += 1
+            else:
+                _invertedIndex[_tokenWord]
+            _invertedIndex[_tokenWord]["list"].append(_docID)
     return _invertedIndex
 
 def tokenize(_document):
     _tokenList = list()
+    #_tokenDict = defaultdict({"bold": false, "title": false, "header":"none"})
     #TODO
+    #   Make sure there are no duplicates
+    #   Do not get rid of stopwords
+    #   Generalize words... with a library?
+    #   Do we need BeautifulSoup again?
     return _tokenList
