632f83cc1131b98dfd0165bdc443e89c7c12ac41
Brian Caballero <briancaballero7@gmail.com>
2023-02-15 17:22:58 -0800

    Initial commit

---
+++ indexer.py
@@ -0,0 +1,13 @@
+
+def createInvertedIndex(_documentSet):
+    _invertedIndex = list()
+    _docID = 0
+    for _document in _documentSet:
+        _docID += 1
+        _tokenList = tokenize(_document)
+    return _invertedIndex
+
+def tokenize(_document):
+    _tokenList = list()
+    #TODO
+    return _tokenList
