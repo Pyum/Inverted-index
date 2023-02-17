a81c7a10636caaa897bd093792c7813b3ae7a81d
Brian Caballero <briancaballero7@gmail.com>
2023-02-17 13:25:34 -0800

    Added txt Files & Code to Write

--- indexer.py
+++ indexer.py
@@ -2,31 +2,33 @@ from collections import defaultdict
 from bs4 import BeautifulSoup
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import *
-import json, os
+import json, os, sys
 
-_invertedIndex = defaultdict({"freq": 1, "list":[], "bold":[], "title":[], "header":{"h1":[], "h2":[],"h3":[]}})
+InvertedIndex = defaultdict({"freq": 1, "list":[], "bold":[], "title":[], "header":{"h1":[], "h2":[],"h3":[]}})
 #                               Frequency | DocIDs In | In Bolded | In Title | In Header  <Header it is in>    |
 #                                                     |In Theory these should be a lot smaller                 |
+UniqueWords = {}
 
-def createInvertedIndex(_documentURL, _documentContent, _docID):
-    global _invertedIndex
+def addInvertedIndex(_documentURL, _documentContent, _docID):
+    global InvertedIndex
     _tokenDict = tokenize(_documentContent)
     for _tokenWord in _tokenDict:
-        if _tokenWord in _invertedIndex:
-            _invertedIndex[_tokenWord]["freq"] += 1     #Add Frequency
+        if _tokenWord in InvertedIndex:
+            InvertedIndex[_tokenWord]["freq"] += 1     #Add Frequency
         else:
-            _invertedIndex[_tokenWord]      #Add to Dict
-        _invertedIndex[_tokenWord]["list"].append(_docID)       #Add DocID (Base List)
+            InvertedIndex[_tokenWord]      #Add to Dict
+        InvertedIndex[_tokenWord]["list"].append(_docID)       #Add DocID (Base List)
         if _tokenWord["bold"]:                  #Is the word in bold in this document?
-            _invertedIndex[_tokenWord]["bold"].append(_docID)   #Add DocID (Bold List)
+            InvertedIndex[_tokenWord]["bold"].append(_docID)   #Add DocID (Bold List)
         if _tokenWord["title"]:               #Is the word in the title in this document?
-            _invertedIndex[_tokenWord]["title"].append(_docID)  #Add DocID (Title List)
+            InvertedIndex[_tokenWord]["title"].append(_docID)  #Add DocID (Title List)
         if len(_tokenWord["header"]) != 0:    #Is the word in the header in this document?
             for _header in _tokenWord["header"]:
-                _invertedIndex[_tokenWord][_header].append(_docID) #Add DocID (Header Dict)
-    return _invertedIndex
+                InvertedIndex[_tokenWord][_header].append(_docID) #Add DocID (Header Dict)
+    return InvertedIndex
 
 def tokenize(_documentContent):
+    global UniqueWords
     _soup = BeautifulSoup(_documentContent, 'html.parser')
     _bTags = [] 
     for i in _soup.findAll('b'):
@@ -61,17 +63,41 @@ def tokenize(_documentContent):
             _tokenDict[_stemmer.stem(_word)]["header"].append("h2") #Checking if word is in a header 2
         if _word in _h3Tags:
             _tokenDict[_stemmer.stem(_word)]["header"].append("h3") #Checking if word is in a header 3
+        UniqueWords.add(_stemmer.stem(_word))
     return _tokenDict
 
 def openJson():
     _docID = 0
     for _folder in os.listdir("DEV"):
         for _file in os.listdir("DEV/" + _folder):
+            _docID += 1
             f = open("DEV/" + _folder + "/" + _file)
             _fileData = json.load(f)
             _fileContent = _fileData["content"]
             _fileURL = _fileData["url"]
-            
+            addInvertedIndex(_fileURL, _fileContent, _docID)
+    return
+
+def main():
+    global UniqueWords, InvertedIndex
+    openJson()
+
+    _uniqueWordsFile = open("UniqueWords.txt", "w")
+    _uniqueWordsFile.write( len(UniqueWords) + " words\n")
+    for i in UniqueWords:
+        _uniqueWordsFile.write( i + "\n")
+    _uniqueWordsFile.close() 
+
+    _invertedIndexFile = open("InvertedIndex.txt", "w")
+    _invertedIndexFile.write( len(InvertedIndex) + " indexes\nWith size of " + sys.getsizeof(InvertedIndex))
+    for keys, value in InvertedIndex.items():
+        _invertedIndexFile.write( keys + " " + value + "\n")
+
+if __name__ == '__main__':
+    main()
+
+
+
             
 
 
