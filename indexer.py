093d9c0f04c78788ebb52728d80e731681f3bd86
Brian Caballero <briancaballero7@gmail.com>
2023-02-17 16:14:32 -0800

    Debug and Test

--- indexer.py
+++ indexer.py
@@ -2,27 +2,28 @@ from collections import defaultdict
 from bs4 import BeautifulSoup
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import *
-import json, os, sys
+import json, os, sys, nltk
 
-InvertedIndex = defaultdict({"freq": 1, "list":[], "bold":[], "title":[], "header":{"h1":[], "h2":[],"h3":[]}})
-#                               Frequency | DocIDs In | In Bolded | In Title | In Header  <Header it is in>    |
-#                                                     |In Theory these should be a lot smaller                 |
-UniqueWords = {}
+InvertedIndex = defaultdict()
+
+UniqueWords = set()
 
 def addInvertedIndex(_documentURL, _documentContent, _docID):
     global InvertedIndex
     _tokenDict = tokenize(_documentContent)
-    for _tokenWord in _tokenDict:
+    for _tokenWord in _tokenDict.keys():
         if _tokenWord in InvertedIndex:
             InvertedIndex[_tokenWord]["freq"] += 1     #Add Frequency
         else:
-            InvertedIndex[_tokenWord]      #Add to Dict
+            InvertedIndex[_tokenWord] = {"freq": 1, "list":[], "bold":[], "title":[], "header":{"h1":[], "h2":[],"h3":[]}}      #Add to Dict
+            #                            Frequency | DocIDs In | In Bolded | In Title | In Header  <Header it is in>    |
+            #                                      |In Theory these should be a lot smaller                             |
         InvertedIndex[_tokenWord]["list"].append(_docID)       #Add DocID (Base List)
-        if _tokenWord["bold"]:                  #Is the word in bold in this document?
+        if _tokenDict[_tokenWord]["bold"]:                  #Is the word in bold in this document?
             InvertedIndex[_tokenWord]["bold"].append(_docID)   #Add DocID (Bold List)
-        if _tokenWord["title"]:               #Is the word in the title in this document?
+        if _tokenDict[_tokenWord]["title"]:               #Is the word in the title in this document?
             InvertedIndex[_tokenWord]["title"].append(_docID)  #Add DocID (Title List)
-        if len(_tokenWord["header"]) != 0:    #Is the word in the header in this document?
+        if len(_tokenDict[_tokenWord]["header"]) != 0:    #Is the word in the header in this document?
             for _header in _tokenWord["header"]:
                 InvertedIndex[_tokenWord][_header].append(_docID) #Add DocID (Header Dict)
     return InvertedIndex
@@ -32,27 +33,34 @@ def tokenize(_documentContent):
     _soup = BeautifulSoup(_documentContent, 'html.parser')
     _bTags = [] 
     for i in _soup.findAll('b'):
-        _bTags.append(j.text for j in i.split(' ')) #Get all bold text
+        if type(i) == str:
+            _bTags.append(j.text for j in i.split(' ')) #Get all bold text
     _tTags = []
     for i in _soup.findAll('title'):
-        _tTags.append(j.text for j in i.split(' ')) #Get all text in title
+        if type(i) == str:
+            _tTags.append(j.text for j in i.split(' ')) #Get all text in title
     _h1Tags = []
     for i in _soup.findAll('h1'):
-        _h1Tags.append(j.text for j in i.split(' ')) #Get all text in all header 1
+        if type(i) == str:
+            _h1Tags.append(j.text for j in i.split(' ')) #Get all text in all header 1
     _h2Tags = []
     for i in _soup.findAll('h2'):
-        _h2Tags.append(j.text for j in i.split(' ')) #Get all text in all header 2
+        if type(i) == str:
+            _h2Tags.append(j.text for j in i.split(' ')) #Get all text in all header 2
     _h3Tags = []
     for i in _soup.findAll('h3'):
-        _h3Tags.append(j.text for j in i.split(' ')) #Get all text in all header 3
+        if type(i) == str:
+            _h3Tags.append(j.text for j in i.split(' ')) #Get all text in all header 3
     _htmlText = _soup.get_text()
-    _tokenList = list()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", _htmlText) # remove all symbols before tokenizing
+    _tokenList = nltk.word_tokenize(text)
+    #_tokenList = list()
     _stemmer = PorterStemmer()
-    _tokenDict = defaultdict({"bold": False, "title": False, "header":[]})
-    _regExp = RegexpTokenizer('[a-zA-Z]+[\'a-zA-Z]*')
-    _tokenList = _regExp.tokenize(_htmlText)
+    _tokenDict = defaultdict()
+    #_regExp = RegexpTokenizer('[a-zA-Z]+[\'a-zA-Z]*')
+    #_tokenList = _regExp.tokenize(_htmlText)
     for _word in _tokenList:
-        _tokenDict = _stemmer.stem(_word) #Gets rid of plurals
+        _tokenDict[_stemmer.stem(_word)] = {"bold": False, "title": False, "header":[]} #Gets rid of plurals
         if _word in _bTags:
             _tokenDict[_stemmer.stem(_word)]["bold"] = True #Checking if word is bold 
         if _word in _tTags:
@@ -69,8 +77,10 @@ def tokenize(_documentContent):
 def openJson():
     _docID = 0
     for _folder in os.listdir("DEV"):
+        print("In folder " + _folder)
         for _file in os.listdir("DEV/" + _folder):
             _docID += 1
+            print("On docID " + str(_docID)) #TODO Delete
             f = open("DEV/" + _folder + "/" + _file)
             _fileData = json.load(f)
             _fileContent = _fileData["content"]
@@ -80,18 +90,24 @@ def openJson():
 
 def main():
     global UniqueWords, InvertedIndex
+    nltk.download('punkt')
     openJson()
 
     _uniqueWordsFile = open("UniqueWords.txt", "w")
-    _uniqueWordsFile.write( len(UniqueWords) + " words\n")
+    _uniqueWordsFile.write( str(len(UniqueWords)) + " words\n")
     for i in UniqueWords:
         _uniqueWordsFile.write( i + "\n")
     _uniqueWordsFile.close() 
 
     _invertedIndexFile = open("InvertedIndex.txt", "w")
-    _invertedIndexFile.write( len(InvertedIndex) + " indexes\nWith size of " + sys.getsizeof(InvertedIndex))
+    _invertedIndexFile.write( str(len(InvertedIndex)) + " indexes\nWith size of " + str(sys.getsizeof(InvertedIndex)))
     for keys, value in InvertedIndex.items():
-        _invertedIndexFile.write( keys + " " + value + "\n")
+        _invertedIndexFile.write( keys)
+        for i in value:
+            _invertedIndexFile.write(" " + i + " ")
+            _invertedIndexFile.write(value[i])
+        _invertedIndexFile.write("\n")
+        
 
 if __name__ == '__main__':
     main()
