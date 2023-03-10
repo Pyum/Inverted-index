e2de81ecce42110cd52926d33a69fc4c41f0c781
Brian Caballero <briancaballero7@gmail.com>
2023-03-09 19:41:39 -0800

    Trying to Optimize

--- indexer.py
+++ indexer.py
@@ -9,21 +9,26 @@ LinkIndex = defaultdict()
 
 UniqueWords = set()
 
-def clearInvertedIndex():
-    global InvertedIndex
-    for _indexName, _indexValue in InvertedIndex.items():
-        _fileName = "indexes/" + str(_indexName[0]) + ".txt"
-        _file = open(_fileName, "a")
-        _file.write(_indexName + ":{")
-        for i in _indexValue:
-            _file.write(i+":"+str(_indexValue[i]))
-            if i != 'header':
-                _file.write(",")
-            else:
-                _file.write("}")
-        _file.write("\n")
-        _file.close()
-    InvertedIndex = defaultdict()
+def clearTxtFiles():
+    for _file in "abcdefghijklmnopqrstuvwxyz":
+        for _subfile in "abcdefghijklmnopqrstuvwxyz":
+            open("indexes/" + _file + "/" + _subfile + ".txt", 'w').close()
+    open("indexes/LinkIndex.txt", 'w').close()
+
+def openJson():
+    global LinkIndex
+    _docID = 0
+    for _folder in os.listdir("DEV"):
+        print("In folder " + _folder)
+        for _file in os.listdir("DEV/" + _folder):
+            _docID += 1
+            print("    On docID #" + str(_docID) + "/55393") #TODO Delete
+            f = open("DEV/" + _folder + "/" + _file)
+            _fileData = json.load(f)
+            _fileContent = _fileData["content"]
+            _fileURL = _fileData["url"]
+            LinkIndex[_docID] = _fileURL
+            addInvertedIndex(_fileURL, _fileContent, _docID)
     return
 
 def addInvertedIndex(_documentURL, _documentContent, _docID):
@@ -46,6 +51,26 @@ def addInvertedIndex(_documentURL, _documentContent, _docID):
                 InvertedIndex[_tokenWord]["header"][_header].append(_docID) #Add DocID (Header Dict)
     return InvertedIndex
 
+def clearInvertedIndex():
+    global InvertedIndex
+    for _indexName, _indexValue in sorted(InvertedIndex.items(), key=lambda x:(x[1]["freq"]), reverse=True):
+        if len(_indexName) == 1:
+            _fileName = "indexes/"+ str(_indexName[0]) + "/0" + ".txt"
+        else:
+            _fileName = "indexes/" + str(_indexName[0]) + "/" + str(_indexName[1])+ ".txt"
+        _file = open(_fileName, "a")
+        _file.write(_indexName + ":{")
+        for i in _indexValue:
+            _file.write(i+":"+str(_indexValue[i]))
+            if i != 'header':
+                _file.write(",")
+            else:
+                _file.write("}")
+        _file.write("\n")
+        _file.close()
+    InvertedIndex = defaultdict()
+    return
+
 def tokenizeQuery(_query):
     _tokenizedQuery = set()
     _stemmer = PorterStemmer()
@@ -63,6 +88,11 @@ def tokenize(_documentContent):
             _tTags.add(_stemmer.stem(i))
     _bTags = set() 
     for i in _soup.findAll('b'):
+        if len(i) > 0:
+            for j in str(i.getText()).split(" "):
+                for k in j.split("\n"):
+                    _bTags.add(_stemmer.stem(k)) #Get all bold text
+    for i in _soup.findAll('strong'):
         if len(i) > 0:
             for j in str(i.getText()).split(" "):
                 for k in j.split("\n"):
@@ -107,25 +137,12 @@ def tokenize(_documentContent):
         UniqueWords.add(_codeWord)
     return _tokenDict
 
-def openJson():
-    global LinkIndex
-    _docID = 0
-    for _folder in os.listdir("DEV"):
-        print("In folder " + _folder)
-        for _file in os.listdir("DEV/" + _folder):
-            _docID += 1
-            print("    On docID #" + str(_docID) + "/55393") #TODO Delete
-            f = open("DEV/" + _folder + "/" + _file)
-            _fileData = json.load(f)
-            _fileContent = _fileData["content"]
-            _fileURL = _fileData["url"]
-            LinkIndex[_docID] = _fileURL
-            addInvertedIndex(_fileURL, _fileContent, _docID)
-    return
-
 def getWordFromIndexes(_word): #word should have already been PorterStemmer()
     _lineNum = 0
-    _fileName = "indexes/" + str(_word[0]) + ".txt"
+    if len(_word) == 1:
+        _fileName = "indexes/"+ str(_word[0]) + "/0" + ".txt"
+    else:
+        _fileName = "indexes/" + str(_word[0]) + "/" + str(_word[1])+ ".txt"
     _file = open(_fileName, "r")
     _regExp = r'(\w+):{freq:([\d]+),list:\[([\d, ]*)],bold:\[([\d, ]*)],title:\[([\d, ]*)],header:{\'h1\': \[([\d, ]*)], \'h2\': \[([\d, ]*)], \'h3\': \[([\d, ]*)]}}' #Do I ahve to escape the \ ??
     while True:
@@ -136,53 +153,70 @@ def getWordFromIndexes(_word): #word should have already been PorterStemmer()
         if _match.group(1) == _word:
             _bold = []
             if len(_match.group(4)) != 0:
-                _bold = [int(_docID) for _docID in _match.group(4).split(', ')]
+                _bold = _match.group(4).split(', ')
             _title = []
             if len(_match.group(5)) != 0:
-                _title = [int(_docID) for _docID in _match.group(5).split(', ')]
+                _title = _match.group(5).split(', ')
             _header = {'h1':[], 'h2':[], 'h3':[]}
             if len(_match.group(6)) != 0:
-                _header['h1'] = [int(_docID) for _docID in _match.group(6).split(', ')]
+                _header['h1'] = _match.group(6).split(', ')
             if len(_match.group(7)) != 0:
-                _header['h2'] = [int(_docID) for _docID in _match.group(7).split(', ')]
+                _header['h2'] = _match.group(7).split(', ')
             if len(_match.group(8)) != 0:
-                _header['h3'] = [int(_docID) for _docID in _match.group(8).split(', ')]
+                _header['h3'] = _match.group(8).split(', ')
             _file.close()
-            return ({_match.group(1): {'freq':int(_match.group(2)), 'list':[int(_docID) for _docID in _match.group(3).split(', ')], 'bold':_bold, 'title':_title, 'header':_header}}, _lineNum)
+            return ({_match.group(1): {'freq':int(_match.group(2)), 'list':_match.group(3).split(', '), 'bold':_bold, 'title':_title, 'header':_header}}, _lineNum)
         _lineNum += 1
     _file.close()
     return ({_word: {"freq": 0, "list":[], "bold":[], "title":[], "header":{"h1":[], "h2":[],"h3":[]}}}, -1)
 
-def intersect (_word1, _word2):
+def intersect(_word1, _word2):
     _finalDict = defaultdict(int)
     _dictofWord1 = getWordFromIndexes(_word1)[0]
     _dictofWord2 = getWordFromIndexes(_word2)[0]
-    for _docID in _dictofWord1[_word1]["list"]:
-            if _docID in _dictofWord2[_word2]["list"]:
-                _finalDict[_docID] = 2
-                if _docID in _dictofWord2[_word2]["bold"]:
-                    _finalDict[_docID] += 15
-                if _docID in _dictofWord2[_word2]["title"]:
-                    _finalDict[_docID] += 100
-                if len(_dictofWord2[_word2]["header"]) != 0:
-                    _headNum = 3
-                    for _header in _dictofWord2[_word2]["header"].values():
-                        if _docID in _header:
-                            _finalDict[_docID] += (25 * _headNum)
-                        _headNum -= 1
-                if _docID in _dictofWord1[_word1]["bold"]:
-                        _finalDict[_docID] += 15
-                if _docID in _dictofWord1[_word1]["title"]:
-                    _finalDict[_docID] += 100
-                if len(_dictofWord1[_word1]["header"]) != 0:
-                    _headNum = 3
-                    for _header in _dictofWord1[_word1]["header"].values():
-                        if _docID in _header:
-                            _finalDict[_docID] += (25 * _headNum)
-                        _headNum -= 1
+    if _dictofWord1[_word1]["freq"] < _dictofWord2[_word2]["freq"]:
+        for _docID in _dictofWord1[_word1]["list"]:
+                if _docID in _dictofWord2[_word2]["list"]:
+                    _finalDict[int(_docID)] = 2
+                    if _docID in _dictofWord2[_word2]["bold"] or _docID in _dictofWord1[_word1]["bold"]:
+                        _finalDict[int(_docID)] += 15
+                    if _docID in _dictofWord2[_word2]["title"] or _docID in _dictofWord1[_word1]["title"]:
+                        _finalDict[int(_docID)] += 100
+                    if len(_dictofWord2[_word2]["header"]) != 0:
+                        _headNum = 3
+                        for _header in _dictofWord2[_word2]["header"].values():
+                            if _docID in _header:
+                                _finalDict[int(_docID)] += (25 * _headNum)
+                            _headNum -= 1
+                    if len(_dictofWord1[_word1]["header"]) != 0:
+                        _headNum = 3
+                        for _header in _dictofWord1[_word1]["header"].values():
+                            if _docID in _header:
+                                _finalDict[int(_docID)] += (25 * _headNum)
+                            _headNum -= 1
+    else:
+        for _docID in _dictofWord2[_word2]["list"]:
+                if _docID in _dictofWord1[_word1]["list"]:
+                    _finalDict[int(_docID)] = 2
+                    if _docID in _dictofWord1[_word1]["bold"] or _docID in _dictofWord2[_word2]["bold"]:
+                        _finalDict[int(_docID)] += 15
+                    if _docID in _dictofWord1[_word1]["title"] or _docID in _dictofWord2[_word2]["title"]:
+                        _finalDict[int(_docID)] += 100
+                    if len(_dictofWord1[_word1]["header"]) != 0:
+                        _headNum = 3
+                        for _header in _dictofWord1[_word1]["header"].values():
+                            if _docID in _header:
+                                _finalDict[int(_docID)] += (25 * _headNum)
+                            _headNum -= 1
+                    if len(_dictofWord2[_word2]["header"]) != 0:
+                        _headNum = 3
+                        for _header in _dictofWord2[_word2]["header"].values():
+                            if _docID in _header:
+                                _finalDict[int(_docID)] += (25 * _headNum)
+                            _headNum -= 1
     return _finalDict
 
-def combineDicts (_dict1, _dict2):
+def combineDicts(_dict1, _dict2):
     _finDict = defaultdict(int)
     if len(_dict2) == 0:
         return _dict1
@@ -191,8 +225,12 @@ def combineDicts (_dict1, _dict2):
             _finDict[i] = _dict1[i] + _dict1[i]
     return _finDict
 
-def getlinks (_listOfDocID):
-    _docID = 0
+def getlinks(_listOfDocID):
+    global LinkIndex
+    _finList = []
+    for _docID in _listOfDocID:
+        _finList.append(LinkIndex[int(_docID)])
+    return _finList
     _finList = []
     for _folder in os.listdir("DEV"):
         for _file in os.listdir("DEV/" + _folder):
@@ -203,34 +241,48 @@ def getlinks (_listOfDocID):
                 _finList.append(_fileData["url"])
     return _finList
 
-def dictToString(_dict): #Should be a 1 item dict :fearful:
-    _finString = ''
-    for keys, value in _dict.items():
-        _finString += keys + ":{"
-        for i in value:
-            _finString += i + ":" + str(value[i])
-            if i != 'header':
-                _finString += ","
-            else:
-                _finString += "}"
-        _finString += "\n"
-    return _finString
+def GetLinkIndexTxt():
+    global LinkIndex
+    _fileName = "indexes/LinkIndex.txt"
+    _file = open(_fileName, "r")
+    while True:
+        _item = _file.readline()
+        if _item == "":
+            break
+        _item = _item.split("->")
+        LinkIndex[int(_item[0])] = _item[1].split("\n")[0]
+    _file.close()
+
+def FillLinkIndexTxt():
+    global LinkIndex
+    _fileName = "indexes/LinkIndex.txt"
+    _file = open(_fileName, "a")
+    for _docID, _link in LinkIndex.items():
+        _file.write(str(_docID) + "->" + str(_link) + "\n")
+    _file.close()
 
 def main():
     global UniqueWords, InvertedIndex
     nltk.download('punkt')
-    makeIndex = False #Ran on 3/5/23 @ 1:17AM Ended @ 2:52AM
+    makeIndex = True #Ran on 3/5/23 @ 1:17AM Ended @ 2:52AM
     askQuery = True
     if makeIndex:
+        clearTxtFiles()
         openJson()
         clearInvertedIndex()
+        FillLinkIndexTxt()
         print("Indexing Complete...")
     if askQuery:
+        print("Loading...")
+        if not makeIndex:
+            GetLinkIndexTxt()
         while True:
             _query = input("What would you like to seach for:\n")
             _startTime = time.time()
             if _query == "end":
                 break
+            if _query[-1] == " ":
+                _query = _query[:-1]
             _setOfQueryWords = tokenizeQuery(_query)
             _listOfQueryWords = [item for item in _setOfQueryWords]
             _finDict = defaultdict(int)
